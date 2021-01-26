7d2b543869b9640b26fb0d148c6edba022a0f899
SandrineLe <le.sandrinemt@gmail.com>
2021-01-26 17:01:29 +0100

    maj

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { getCode } = require("country-list");
 const axios = require('axios').default;
 const figlet = require('figlet');
 const chalk = require('chalk');
-const ora = require('ora');
 const myArgs = process.argv.slice(2);
  
 figlet('Holidays!', function(err, data) {
@@ -18,3 +17,29 @@ figlet('Holidays!', function(err, data) {
 
 console.log(chalk.cyan('Check your national...'));
 
+let year;
+if (myArgs[1]) {
+    year = myArgs[1]
+} else {
+    year = new Date().getFullYear();
+}
+
+axios.get(`https://date.nager.at/api/v2/publicholidays/${year}/${getCode(myArgs[0])}`)
+.then(function (response) {
+    figlet(`↓`, (err, data) => {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+        response.data.forEach(holiday => {
+            console.log(`${chalk.cyan(holiday.date)} - ${holiday.name}`);
+        });
+    });
+    
+    
+})
+.catch(function (error) {
+    console.log(`An error occured: ${error.response.status} - ${error.response.statusText}`);
+});
