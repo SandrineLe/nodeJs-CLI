449beff667e5ced5bff5f6681117363c983a987d
SandrineLe <le.sandrinemt@gmail.com>
2021-01-26 16:54:25 +0100

    maj

--- index.js
+++ index.js
@@ -18,34 +18,3 @@ figlet('Holidays!', function(err, data) {
 
 console.log(chalk.cyan('Check your national...'));
 
-const spinner = ora('Loading').start();
-
-
-if(myArgs.length != 2 && myArgs.length != 1){
-    spinner.stop();
-    console.error(chalk.red("Something went wrong..."));
-    return;
-}
-
-let country = myArgs[0];
-let code = getCode(country);
-
-let year = new Date().getFullYear();
-
-if(myArgs.length == 2){
-    year = Number(myArgs[1]);
-}
-
-axios.get('https://date.nager.at/api/v2/PublicHolidays/'+year+'/'+code)
-  .then(function (response) {
-    spinner.stop();
-    response.data.forEach(holiday => {
-        console.log(holiday.name + holiday.date);
-    });
-   
-  })
-  .catch(function (error) {
-    spinner.stop();
-    console.error(chalk.red('Something went wrong...'));
-  })
-  
